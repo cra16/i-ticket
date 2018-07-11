75693f312b441c7e77e87645a25daee25eb7d115
inthewalter <21400564@handong.edu>
2018-07-12 01:10:15 +0900

    back button 수정 중

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
+// Module
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule, NavController } from 'ionic-angular';
-
-// Firebase
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AuthProvider } from '../providers/auth/auth';
-import { TicketProvider } from '../providers/ticket/ticket' ;
-import { MyApp } from './app.component';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { GooglePlus } from '@ionic-native/google-plus';
-
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
 import { Camera } from '@ionic-native/camera';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import * as firebase from 'firebase';
-
+import { GooglePlus } from '@ionic-native/google-plus';
+// Providers
+import { AuthProvider } from '../providers/auth/auth';
+import { TicketProvider } from '../providers/ticket/ticket' ;
+import { StarProvider } from '../providers/star/star';
+import { UserProvider } from '../providers/user/user';
+// Page
+import { MyApp } from './app.component';
 import { MyListPage } from '../pages/my-list/my-list';
 import { ListPage } from '../pages/list/list';
 import { LoginPage } from '../pages/login/login';
@@ -29,13 +26,15 @@ import { SellerResetPwdPage } from '../pages/seller-reset-pwd/seller-reset-pwd';
 import { SellerEditPage } from '../pages/seller-edit/seller-edit';
 import { TicketInfoPage } from '../pages/ticket-info/ticket-info';
 import { SellerTicketManagePage } from '../pages/seller-ticket-manage/seller-ticket-manage';
-import { UserProvider } from '../providers/user/user';
 import { SellerMainPage } from '../pages/seller-main/seller-main';
-import { StarProvider } from '../providers/star/star';
 import { StarReviewComponent } from '../components/star-review/star-review';
-import { ScreenOrientation } from '@ionic-native/screen-orientation';
 import { SellerMyListPage } from '../pages/seller-my-list/seller-my-list';
 import { SellerSettingPage } from '../pages/seller-setting/seller-setting';
+
+// Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { firebaseKey } from '../key/firebaseKey';
 export const firebaseConfig = {
@@ -62,6 +61,7 @@ export const firebaseConfig = {
     TicketInfoPage,
     SellerTicketManagePage,
     SellerMainPage,
+    SellerResetPwdPage,
     StarReviewComponent,
     SellerSettingPage,
     SellerMyListPage
@@ -72,15 +72,14 @@ export const firebaseConfig = {
       menuType: 'overlay',
       pageTransition: 'ios-transition',
       backButtonIcon: 'md-arrow-back',
-      // // iconMode: 'ios',
+      backButtonText: '',
+      iconMode: 'md',
       // modalEnter: 'modal-slide-in',
       // modalLeave: 'modal-slide-out',
       // tabbarPlacement: 'bottom',
-      // backButtonText: 'BACK',
     }),
-    AngularFireDatabaseModule,
     AngularFireModule.initializeApp(firebaseConfig),
-    AngularFirestoreModule
+    AngularFirestoreModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
