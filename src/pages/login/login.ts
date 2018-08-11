593488605c56bd45208bd6608d25b5828d59ecf9
inthewalter <21400564@handong.edu>
2018-08-12 02:04:57 +0900

    [auth] 구글로그인했을 때, 바로 메인페이지로 리다이렉트

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -4,17 +4,24 @@ import { NavController, NavParams, MenuController, AlertController } from 'ionic
 import { ScreenOrientation } from '@ionic-native/screen-orientation';
 // Page
 import { SellerLoginPage } from '../seller-login/seller-login';
+import { MainPage } from '../main/main';
 // Provider
 import { AuthProvider } from '../../providers/auth/auth';
+import { UserProvider } from '../../providers/user/user';
+import { SellerMainPage } from '../seller-main/seller-main';
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
+
 export class LoginPage {
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              public alertCtrl: AlertController, public menu: MenuController,
-              public auth: AuthProvider, private screen: ScreenOrientation) {
+    public alertCtrl: AlertController, public menu: MenuController,
+    public authProvider: AuthProvider, private screen: ScreenOrientation,
+    public userProvider: UserProvider) {
+      
     // Lock vertical screen
     // 네이티브에서만 적용되는 기능,
     // 마지막에 주석해제 하면 됨.
@@ -24,7 +31,7 @@ export class LoginPage {
     // this.menu.enable(false,'myMenu')
     // 위의 문장을 주석 처리하면 Sidemenu가 사용 가능해짐
   }
-
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
@@ -32,5 +39,10 @@ export class LoginPage {
   // 판매자 로그인 페이지로 이동
   goToSellerLoginPage() {
     this.navCtrl.push(SellerLoginPage) ;
+  }
+
+  googleLogin() {
+    if (this.authProvider.googleLogin())
+      this.navCtrl.setRoot(MainPage);
   }
 }
